81095e94075f3fe9450787a3ec9e3d8976e2d711
srini <srinivasgowda1991@gmail.com>
2018-07-19 20:47:09 +0530

    dependency barreling

--- srini/AngularLearn/src/main/webapp/ng-fundamentals/src/app/app.module.ts
+++ srini/AngularLearn/src/main/webapp/ng-fundamentals/src/app/app.module.ts
@@ -1,15 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  NgModule
+} from '@angular/core';
 
+import {
+  EventListComponent,
+  EventthumbnailComponent,
+  NavComponent,
+  EventDetailComponent,
+  CreateEventComponent,
+  Err404Component
+} from './components/index';
 import { EventsAppComponent } from './eventsapp.component';
-import { EventListComponent } from './components/events/events-list.component';
-import { EventthumbnailComponent } from './components/eventthumbnail/eventthumbnail.component';
-import { NavComponent } from './components/nav/nav.component';
-import { EventDetailComponent } from './components/event-detail/event-detail.component';
-import { RouterModule } from '../../node_modules/@angular/router';
-import { AppRoute } from './routes/app.routes';
-import { CreateEventComponent } from './components/create-event/create-event.component';
-import { Err404Component } from './components/errors/err404/err404.component';
+import {
+  RouterModule
+} from '../../node_modules/@angular/router';
+import {
+  AppRoute
+} from './routes/app.routes';
+
 
 @NgModule({
   declarations: [
---
+++ srini/AngularLearn/src/main/webapp/ng-fundamentals/src/app/components/index.ts
@@ -0,0 +1,6 @@
+export * from './create-event/create-event.component';
+export * from './errors/err404/err404.component';
+export * from './event-detail/event-detail.component';
+export * from './events/events-list.component';
+export * from './eventthumbnail/eventthumbnail.component';
+export * from './nav/nav.component';
